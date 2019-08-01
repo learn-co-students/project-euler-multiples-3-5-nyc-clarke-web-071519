2436b4145ee65f069f21d8c9ab0fbcdf6078df3c
github username <github email address>
2019-08-01 09:52:02 -0400

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,24 @@
 # Enter your object-oriented solution here!
+def collect_multiples(limit)
+    arr = []
+    num = 1
+    while num < limit
+        if num % 3 == 0 || num % 5 == 0
+            arr << num
+        end 
+        num += 1
+    end 
+    arr
+end 
+
+def sum_multiples(limit)
+   sum = 0
+    num = 1
+    while num < limit
+        if num % 3 == 0 || num % 5 == 0
+            sum += num
+        end 
+        num += 1
+    end 
+    sum
+end 
