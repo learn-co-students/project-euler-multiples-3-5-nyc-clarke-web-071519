eadb4eea2ed6ff4bbb87dae3ca4d9db7226d86e8
Meghannwalsh <meghann.mccool.walsh@gmail.com>
2019-08-01 09:49:29 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,36 @@
 # Enter your procedural solution here!
+class Multiples
+
+    def initialize(limit)
+        @limit = limit
+    end 
+
+    def collect_multiples
+        arr = []
+        num = 1
+        while num < @limit
+            if num % 3 == 0 || num % 5 == 0
+                arr << num
+            end 
+            num += 1
+        end 
+         arr
+    end 
+
+    def sum_multiples
+        sum = 0
+        num = 1
+        while num < @limit
+            if num % 3 == 0 || num % 5 == 0
+                sum += num
+            end 
+            num += 1
+        end 
+        sum
+    end 
+        
+end 
+
+ 
+
+
